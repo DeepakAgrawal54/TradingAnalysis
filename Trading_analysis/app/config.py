6b59f13cf6d70e956b4e1591dde8f8e60ab1e220
DeepakAgrawal54 <iamdeepak2095@gmail.com>
2025-02-27 13:14:09 +0000

    Update config.py

--- Trading_analysis/app/config.py
+++ Trading_analysis/app/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     reddit_client_secret: str
     cache_ttl: int = 3600
     groq_api_key: str
+    financial_modeling_key: str
     
     class Config:
         env_file = ".env"
